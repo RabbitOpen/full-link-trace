ccb53d975bb7a397ed4b2f30991d7e25bb7b01f4
RabbitOpen <709498191@qq.com>
2023-11-20 19:55:28 +0800

    init

--- flt-rpc/flt-rpc-common/src/main/java/rabbit/flt/rpc/common/exception/AuthenticationFailedException.java
+++ flt-rpc/flt-rpc-common/src/main/java/rabbit/flt/rpc/common/exception/AuthenticationFailedException.java
@@ -1,12 +1,14 @@
 package rabbit.flt.rpc.common.exception;
 
+import rabbit.flt.common.utils.StringUtils;
+
 /**
  * 认证失败
  */
 public class AuthenticationFailedException extends AuthenticationException {
 
     public AuthenticationFailedException(Throwable cause) {
-        super("authentication failed! ".concat(cause.getMessage()));
+        super(StringUtils.isEmpty(cause.getMessage()) ? "authentication failed! " : "authentication failed! ".concat(cause.getMessage()));
     }
 
 }
