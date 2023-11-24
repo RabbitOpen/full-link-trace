ee02e7dd9f7ce86a7c750224dd7f9434007b3989
RabbitOpen <709498191@qq.com>
2023-11-24 20:17:54 +0800

    init

--- flt-core/src/test/java/rabbit/flt/core/test/FltCoreTest.java
+++ flt-core/src/test/java/rabbit/flt/core/test/FltCoreTest.java
@@ -6,12 +6,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import rabbit.flt.common.AbstractConfigFactory;
 import rabbit.flt.common.exception.FltException;
-import rabbit.flt.common.utils.VersionUtils;
 import rabbit.flt.core.factory.DefaultConfigFactory;
-import rabbit.flt.core.loader.DefaultPluginClassLoader;
-
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 
 @RunWith(JUnit4.class)
 public class FltCoreTest {
@@ -35,17 +30,4 @@ public class FltCoreTest {
 
     }
 
-    @Test
-    public void classLoaderTest() throws IOException, ClassNotFoundException, InvocationTargetException, IllegalAccessException {
-        String baseDir = VersionUtils.getProperty("flt.properties", "baseDir");
-        String path = baseDir + "/../flt-plugins/logback-support-plugin/target/logback-support-plugin-" + VersionUtils.getVersion() + ".jar";
-        DefaultPluginClassLoader classLoader = new DefaultPluginClassLoader() {
-            @Override
-            protected String getAgentJarFilePath() {
-                return path;
-            }
-        };
-        Class<?> clz = classLoader.loadClassByName("rabbit.flt.plugins.logback.plugin.LineNumberPlugin");
-        TestCase.assertNotNull(clz);
-    }
 }
--- flt-test-cases/spring-web-flux-2.7.2-starter/src/test/java/rabbit/flt/test/SpringWebFluxStarterTest.java
+++ flt-test-cases/spring-web-flux-2.7.2-starter/src/test/java/rabbit/flt/test/SpringWebFluxStarterTest.java
@@ -5,11 +5,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import rabbit.flt.common.utils.VersionUtils;
+import rabbit.flt.core.loader.DefaultPluginClassLoader;
 import rabbit.flt.test.cases.BaseCases;
 import rabbit.flt.test.common.ignore.IgnorePackage;
 import rabbit.flt.test.common.service.IgnoreService;
 import rabbit.flt.test.starter.ClassProxyLogger;
 import rabbit.flt.test.starter.SpringBootStartListener;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringWebFluxEntry.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -38,9 +46,26 @@ public class SpringWebFluxStarterTest extends BaseCases {
         TestCase.assertFalse(ClassProxyLogger.getClassMap().containsKey(IgnoreService.class.getName()));
         TestCase.assertFalse(ClassProxyLogger.getClassMap().containsKey(IgnorePackage.class.getName()));
         util.monoDeferCase();
+
+        classLoaderTest();
     }
 
-
+    private void classLoaderTest() throws IOException, ClassNotFoundException, InvocationTargetException, IllegalAccessException {
+        String baseDir = VersionUtils.getProperty("flt.properties", "baseDir");
+        URLClassLoader classLoader = new URLClassLoader(new URL[]{
+                new File(baseDir + "/../flt-core/target").toURI().toURL(),
+        }, ClassLoader.getSystemClassLoader());
+        Thread.currentThread().setContextClassLoader(classLoader);
+        String jarPath = baseDir + "/../flt-core/target/flt-core-" + VersionUtils.getVersion() + ".jar";
+        DefaultPluginClassLoader pluginClassLoader = new DefaultPluginClassLoader() {
+            @Override
+            protected String getAgentJarFilePath() {
+                return jarPath;
+            }
+        };
+        Class<?> clz = pluginClassLoader.loadClassByName("rabbit.flt.plugins.logback.plugin.LineNumberPlugin");
+        TestCase.assertNotNull(clz);
+    }
 
 
 }
